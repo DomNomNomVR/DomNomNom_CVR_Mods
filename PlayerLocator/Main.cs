475c62f53eea9abfc0c958a7c632d6116bed71d3
DomNomNomVR <108088541+DomNomNomVR@users.noreply.github.com>
2023-04-17 13:29:06 +1200

    Added hello world PlayerLocator mod

---
+++ PlayerLocator/Main.cs
@@ -0,0 +1,35 @@
+﻿using ABI_RC.Core.Player;
+using ABI_RC.Core.Savior;
+using ABI_RC.Core.Util;
+using HarmonyLib;
+using MelonLoader;
+using UnityEngine;
+
+namespace PlayerLocator;
+
+public class PlayerLocator : MelonMod {
+
+    private static MelonPreferences_Category _melonCategory;
+    internal static MelonPreferences_Entry<bool> LocateOnJoiningOthers;
+    internal static MelonPreferences_Entry<float> UnfollowDistance;
+
+    public override void OnInitializeMelon() {
+        // Melon Config
+        _melonCategory = MelonPreferences.CreateCategory(nameof(PlayerLocator));
+
+        LocateOnJoiningOthers = _melonCategory.CreateEntry("LocateOnJoiningOthers", true,
+            description: "Whether to locate the person you just joined off.");
+
+        UnfollowDistance = _melonCategory.CreateEntry("UnfollowDistance", 2f,
+            description: "Stop locating the target player once you are this distance away.");
+    }
+
+    public override void OnUpdate() {
+        MelonLogger.Msg("HELLO WORLDDDDDD :D");
+    }
+
+    [HarmonyPatch]
+    private static class HarmonyPatches {
+
+    }
+}
---
+++ PlayerLocator/README.md
@@ -0,0 +1,16 @@
+# PlayerLocator
+
+Find a person in a lobby by showing you an arrow towards their location.
+
+## Configuration
+
+- TODO
+
+## Disclosure
+
+> ---
+> ⚠️ **Notice!**  
+>
+> This mod's developer(s) and the mod itself, along with the respective mod loaders, have no affiliation with ABI!
+>
+> ---
