6ae67a5eff1e5b051466b64eb3b729d5a0a81cc5
DomNomNomVR <108088541+DomNomNomVR@users.noreply.github.com>
2023-04-17 20:54:54 +1200

    do rudimentary implementation.

--- PlayerLocator/Main.cs
+++ PlayerLocator/Main.cs
@@ -5,6 +5,7 @@ using HarmonyLib;
 using MelonLoader;
 using UnityEngine;
 
+
 namespace PlayerLocator;
 
 public class PlayerLocator : MelonMod {
@@ -12,6 +13,8 @@ public class PlayerLocator : MelonMod {
     private static MelonPreferences_Category _melonCategory;
     internal static MelonPreferences_Entry<bool> LocateOnJoiningOthers;
     internal static MelonPreferences_Entry<float> UnfollowDistance;
+
+
 
     public override void OnInitializeMelon() {
         // Melon Config
@@ -25,7 +28,64 @@ public class PlayerLocator : MelonMod {
     }
 
     public override void OnUpdate() {
-        MelonLogger.Msg("HELLO WORLDDDDDD :D");
+        
+        if (!PlayerSetup.Instance) return;
+        var avi = PlayerSetup.Instance._avatar;
+        if (!avi) return;
+        Transform playerRoot = avi.transform;
+        if (!playerRoot) return;
+        Transform compassTransform = playerRoot.Find("PlayerLocatorCompass");
+        if (compassTransform == null) {
+            GameObject compass = new GameObject("PlayerLocatorCompass");
+            compass.layer = LayerMask.NameToLayer("UI Internal");
+            compassTransform = compass.transform;
+            compassTransform.parent = playerRoot;
+            compassTransform.localPosition = 1.2f * Vector3.forward + .2f * Vector3.up;
+        }
+
+        List<CVRPlayerEntity> targets = CVRPlayerManager.Instance.NetworkPlayers;
+
+        // Create a needle for each player to be located.
+        foreach (CVRPlayerEntity target in targets) {
+            string needleName = $"CompassNeedle-{target.Username}";
+            Transform needleTransform = compassTransform.Find(needleName);
+            if (!needleTransform) {
+                GameObject needle = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+                needle.name = needleName;
+                needleTransform = needle.transform;
+                needleTransform.parent = compassTransform;
+                needleTransform.localPosition = Vector3.zero;
+            }
+            needleTransform.LookAt(target.PlayerObject.transform);
+        }
+
+        // Cleanup compass needles that are no longer wanted.
+
+        //
+        //_visualizer.name = "[TheClapper] Visualizer";
+        //_visualizer.layer = LayerMask.NameToLayer("UI Internal");
+        //_visualizer.transform.position = transform.position;
+        //_visualizer.transform.rotation = transform.rotation;
+        //_visualizer.transform.localScale = Vector3.one * MinimumDistance * 2;
+
+        //_visualizer.transform.SetParent(transform, true);
+
+        //PlayerSetup
+
+        //PlayerSetup.Instance.eyeMovement.isLocal;
+        //PlayerSetup.Instance._avatarDescriptor != null 
+        //!string.IsNullOrEmpty(MetaPort.Instance.currentAvatarGuid) && avatarId == MetaPort.Instance.currentAvatarGuid
+
+        /*
+        foreach (CVRPlayerEntity player in CVRPlayerManager.Instance.NetworkPlayers)        {
+            if (player.Username == MetaPort.Instance.username) {
+                MelonLogger.Msg($"found self: {player.Username}");
+            }
+            else {
+                MelonLogger.Msg($"found other: {player.Username}");
+            }
+        }
+        */
     }
 
     [HarmonyPatch]
